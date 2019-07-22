0fc1d3581ef2250e9d88095a693191d3fa350b3c
thiagomacedo <tl.macedo@hotmail.com>
2019-07-22 19:17:18 -0400

    auto-commit

--- src/main/java/br/com/tlmacedo/cafeperfeito/Main.java
+++ src/main/java/br/com/tlmacedo/cafeperfeito/Main.java
@@ -1,6 +1,7 @@
 package br.com.tlmacedo.cafeperfeito;
 
 import br.com.tlmacedo.cafeperfeito.service.ServiceVariaveisSistema;
+import br.com.tlmacedo.cafeperfeito.view.ViewLogin;
 import br.com.tlmacedo.cafeperfeito.view.ViewPrincipal;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -11,7 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         new ServiceVariaveisSistema().getVariaveisSistema();
 
-        new ViewPrincipal().openViewPrincipal();
+        new ViewLogin().openViewLogin(false);
+
+//        new ViewPrincipal().openViewPrincipal();
 
     }
 }
---
+++ src/main/java/br/com/tlmacedo/cafeperfeito/controller/ControllerLogin.java
@@ -0,0 +1,42 @@
+package br.com.tlmacedo.cafeperfeito.controller;
+
+import br.com.tlmacedo.cafeperfeito.interfaces.ModeloCafePerfeito;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerLogin implements Initializable, ModeloCafePerfeito {
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @Override
+    public void fechar() {
+
+    }
+
+    @Override
+    public void criarObjetos() {
+
+    }
+
+    @Override
+    public void preencherObjetos() {
+
+    }
+
+    @Override
+    public void fatorarObjetos() {
+
+    }
+
+    @Override
+    public void escutarTecla() {
+
+    }
+}
