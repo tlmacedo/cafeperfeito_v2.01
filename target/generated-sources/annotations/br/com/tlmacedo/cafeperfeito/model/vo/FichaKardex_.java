09dc3cf997771763ce1d14e2a1a1376a854e1a2b
thiagomacedo <tl.macedo@hotmail.com>
2020-03-09 09:24:27 -0400

    auto-commit

--- target/generated-sources/annotations/br/com/tlmacedo/cafeperfeito/model/vo/FichaKardex_.java
+++ target/generated-sources/annotations/br/com/tlmacedo/cafeperfeito/model/vo/FichaKardex_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2020-02-19T20:51:48")
+@Generated(value="EclipseLink-2.7.0.v20170811-rNA", date="2020-03-06T17:55:07")
 @StaticMetamodel(FichaKardex.class)
 public class FichaKardex_ { 
 
