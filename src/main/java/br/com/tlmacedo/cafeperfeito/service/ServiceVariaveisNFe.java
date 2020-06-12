1233f0a9460d17e33f28272179a90c21e8ae3e80
thiagomacedo <tl.macedo@hotmail.com>
2020-06-11 20:24:33 -0400

    auto-commit

---
+++ src/main/java/br/com/tlmacedo/cafeperfeito/service/ServiceVariaveisNFe.java
@@ -0,0 +1,38 @@
+package br.com.tlmacedo.cafeperfeito.service;
+
+//import br.com.tlmacedo.cafeperfeito.xsd.sistema.config.TConfig;
+
+import br.com.cafeperfeito.xsd.configNFe.config.Ide;
+import br.com.cafeperfeito.xsd.configNFe.config.MyInfNfe;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
+public class ServiceVariaveisNFe {
+
+    public static MyInfNfe MYINFNFE;
+    public static List<Ide.NatOps.NatOp> NATOPS;
+    public static List<Ide.IndPags.IndPag> INDPAGS;
+    public static List<Ide.Mods.Mod> MODS;
+
+    private FileReader arqConfgSistema = null;
+
+    public ServiceVariaveisNFe() throws FileNotFoundException {
+        arqConfgSistema = new FileReader(getClass().getClassLoader().getResource("configNFe.xml").getFile());
+    }
+
+    public void getVariaveisNFe() {
+        try {
+            String xml = ServiceUtilXml.FileXml4String(arqConfgSistema);
+            MYINFNFE = ServiceUtilXml.xmlToObject(xml, MyInfNfe.class);
+            NATOPS = MYINFNFE.getIde().getNatOps().getNatOp();
+            INDPAGS = MYINFNFE.getIde().getIndPags().getIndPag();
+            MODS = MYINFNFE.getIde().getMods().getMod();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
